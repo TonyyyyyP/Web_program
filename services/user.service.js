3f63f866318c59cc0774fb39c8a2c721fe2a7146
Tringuyenquang20 <tringuyenquang20@gmail.com>
2024-12-23 23:06:24 +0700

    Add update and delete functions for user

--- services/user.service.js
+++ services/user.service.js
@@ -1,11 +1,41 @@
 import db from "../utils/db.js";
 
 export default {
+  getAllUsers() {
+  return db("user").whereNull("deletedAt");
+},
+
   add(entity) {
     return db("users").insert(entity);
   },
 
   findByUsername(username) {
     return db("users").where("username", username).first();
+  },
+
+  getUserById(id) {
+    return db("user").where("id", id).first();
+  },
+
+  updateUser(id, username, name, phoneNumber, permission) {
+    const updatedFields = {};
+
+    if (username) updatedFields.username = username;
+    if (name) updatedFields.name = name;
+    if (phoneNumber) updatedFields.phoneNumber = phoneNumber;
+    if (permission) updatedFields.permission = permission;
+
+    return db("user")
+      .where("id", id)
+      .update(updatedFields)
+      .then((count) => count)
+      .catch((error) => {
+        console.error("Lỗi khi cập nhật người dùng:", error);
+        throw new Error("Không thể cập nhật người dùng.");
+      });
+  },
+
+  deleteById(id) {
+    return db("user").where("id", id).update({ deletedAt: db.fn.now() });
   },
 };
