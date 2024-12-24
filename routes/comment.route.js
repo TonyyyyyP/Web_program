69610ad353cc78c4e33c61ae12550edeb4a792a8
Tringuyenquang20 <tringuyenquang20@gmail.com>
2024-12-25 03:09:37 +0700

    Add & update comment functions

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import commentService from "../old_folder/old_services/category.service.js";
+import commentService from "../services/comment.service.js";
+import multer from "multer";
+const upload = multer();
 
 const router = express.Router();
 
@@ -8,16 +10,37 @@ router.get("/comments", async function (req, res) {
   res.render("Comment/list", { comments });
 });
 
-router.post("/comments/add", async function (req, res) {
-  const newComment = {
-    comment: req.body.comment,
-    publishedDay: req.body.publishedDay,
-    user_id: req.body.user_id,
-    article_id: req.body.article_id,
-  };
-  await commentService.addComment(newComment);
-  res.redirect("/comments");
+router.post("/addComment", upload.none(), async function (req, res) {
+  try {
+    const { userId, articleId, comment } = req.body;
+    console.log(userId);
+    console.log(articleId);
+    console.log(comment);
+    if (!userId || !articleId || !comment) {
+      return res
+        .status(400)
+        .json({ message: "Vui lòng cung cấp đầy đủ thông tin!" });
+    }
+
+    const newComment = {
+      comment,
+      publishedDay: new Date(),
+      user_id: userId,
+      article_id: articleId,
+    };
+
+    const addedCommentId = await commentService.addComment(newComment);
+
+    res.status(201).json({
+      message: "Bình luận đã được thêm thành công!",
+      commentId: addedCommentId[0],
+    });
+  } catch (error) {
+    console.error("Error details:", error);
+    res.status(500).json({ message: "Lỗi khi thêm bình luận.", error });
+  }
 });
+
 
 router.post("/comments/delete", async function (req, res) {
   const id = parseInt(req.body.id, 10);
